220327a39cd221b0c13082dd0893a875f37d9052
claudio-code <suporte2424@outlook.com>
2021-09-23 20:02:07 -0300

    added deterrents methods to break line in console print

--- CPlusPlus/helloword.cpp
+++ CPlusPlus/helloword.cpp
@@ -2,5 +2,7 @@
 
 int main()
 {
-    std::cout << "Hello World";
+    std::cout << "Hello World \n";
+    std::cout << "the brave new word" << std::endl;
+    system("read -p 'Press Enter to continue...' var");
 }
