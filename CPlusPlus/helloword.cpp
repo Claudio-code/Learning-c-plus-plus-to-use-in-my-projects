4780846a3541c8cbb43ea37501a27631af69a777
claudio-code <suporte2424@outlook.com>
2021-09-25 03:48:34 -0300

    added file importation

--- CPlusPlus/helloword.cpp
+++ CPlusPlus/helloword.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
+#include "variableDeclaration.cpp"
 
-int main()
+void helloWord()
 {
     std::cout << "Hello World \n";
     std::cout << "the brave new word" << std::endl;
-    system("read -p 'Press Enter to continue...' var");
+}
+
+int main()
+{
+    helloWord();
+    variableDeclaration();
 }
---
+++ CPlusPlus/variableDeclaration.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int variableDeclaration()
+{
+    int yearsOld, yearsOldTwo = 20;
+    yearsOld = 25;
+
+    std::cout << yearsOld;
+
+    return 0;
+}
