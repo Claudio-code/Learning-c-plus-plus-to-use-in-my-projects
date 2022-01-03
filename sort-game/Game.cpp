c4c1acee57e0a3701d3ff2799257c1d37e568ce6
claudio-code <suporte2424@outlook.com>
2022-01-02 22:50:35 -0300

    added validation of kick made of user

--- sort-game/Game.cpp
+++ sort-game/Game.cpp
@@ -1,14 +1,46 @@
 #include "ShowMessages.cpp"
 
 class Game {
+    private:
+        int secretNumber = 0;
+        int kickedNumber = 0;
+
     public:
         Game() {
+            this->secretNumber = rand() % 100;
+            this->start();
+        }
+
+        void start() {
             Game::presentationMessage();
+            this->kickNumber();
+            this->validateKick();
         }
 
         static void presentationMessage() {
             ShowMessages::printMessages("***********************************");
             ShowMessages::printMessages("* Welcome to the guessing game !! *");
             ShowMessages::printMessages("***********************************");
+        }
+
+        void kickNumber() {
+            ShowMessages::printMessages("kick a number = ");
+            cin >> this->kickedNumber;
+
+            ShowMessages::printMessages("the value of your kick is = " + to_string(this->kickedNumber));
+        }
+
+        void validateKick() const {
+            if (this->kickedNumber == this->secretNumber) {
+                ShowMessages::printMessages("Good !! You got it right te secret number is = " + to_string(this->kickedNumber));
+                return;
+            }
+
+            if (this->kickedNumber > this->secretNumber) {
+                ShowMessages::printMessages("Your kicked is bigger than the secret number");
+                return;
+            }
+
+            ShowMessages::printMessages("Your kicked is smaller than the secret number");
         }
 };
